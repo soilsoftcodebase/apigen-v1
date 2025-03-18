f22f0b768e127df82bd6229c67c97599c8351e63
soilsoftcodebase <vamsikrishna.nekkanti@soilsoft.ai>
2025-03-18 11:59:38 +0530

    Update config.js

--- src/API/config.js
+++ src/API/config.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
 const API_URL =
   // process.env.APIGEN_API_BASE_URL ||
-  "https://apigenbackend.soilsoft.ai:5001/api";
+  "https://apigenapi.soilsoft.ai/api";
   export default API_URL;
